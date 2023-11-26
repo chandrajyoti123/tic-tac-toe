030b6a91eba92d0217a87950888eaf833b5b1ae0
chandrajyoti123 <chandrajyotiadil15062003@gmail.com>
2023-11-27 01:14:32 +0530

    all the logic implement

---
+++ src/Home/Home.js
@@ -0,0 +1,151 @@
+import React, { useEffect, useState } from 'react'
+import './Home.css'
+
+export default function Home() {
+    const [player,setPlayer]=useState(1)
+    const [board, setBoard]=useState({
+        1:"",
+        2:"",
+        3:"",
+        4:"",
+        5:"",
+        6:"",
+        7:"",
+        8:"",
+        9:"",
+
+    })
+    // useEffect(()=>{
+    //     setWinner('')
+    // },[])
+    const [winner,setWinner]=useState("")
+    useEffect(()=>{
+        getwinner()
+     
+        setPlayer(player===1?2:1)
+    },[board])
+
+    const getwinner=()=>{
+        const symbol= player==1?"🦁":"🐵"
+        if(board[1]==symbol && board[2]==symbol && board[3]==symbol){
+            // alert(`player who have symbol ${symbol} is win `)
+            setWinner(player)
+            
+        }
+        else if(board[4]==symbol && board[5]==symbol && board[6]==symbol){
+            // alert(`player who have symbol ${symbol} is win `)
+            setWinner(player)
+        }
+        else if(board[7]==symbol && board[8]==symbol && board[9]==symbol){
+            // alert(`player who have symbol ${symbol} is win `)
+            setWinner(player)
+        }
+        else if(board[1]==symbol && board[4]==symbol && board[7]==symbol){
+            // alert(`player who have symbol ${symbol} is win `)
+            setWinner(player)
+        }
+        else if(board[2]==symbol && board[5]==symbol && board[8]==symbol){
+            // alert(`player who have symbol ${symbol} is win `)
+            setWinner(player)
+        }
+        else if(board[3]==symbol && board[6]==symbol && board[9]==symbol){
+            // alert(`player who have symbol ${symbol} is win `)
+            setWinner(player)
+        }
+        else if(board[1]==symbol && board[5]==symbol && board[9]==symbol){
+            // alert(`player who have symbol ${symbol} is win `)
+            setWinner(player)
+        }
+        else if(board[3]==symbol && board[5]==symbol && board[7]==symbol){
+            // alert(`player who have symbol ${symbol} is win `)
+            setWinner(player)
+        }
+        else{
+            return;
+        }
+
+    }
+    const play=(box)=>{
+        if(board[box] != "" ||  winner != ""){
+            return
+        }
+        if(player===1){
+            setBoard({...board, [box]:"🦁"})
+        }
+        else{
+            setBoard({...board, [box]:"🐵"})
+
+        }
+       
+
+    }
+    const reset=()=>{
+        setWinner('')
+        setPlayer(1)
+        setBoard({
+            1:"",
+            2:"",
+            3:"",
+            4:"",
+            5:"",
+            6:"",
+            7:"",
+            8:"",
+            9:"",
+        })
+    }
+  return (
+    <div>
+        <h1 className='heading'>tic tac toe</h1>
+        <div className='players'>
+            <div>player one🦁</div>
+            <div>player two🐵</div>
+
+        </div>
+        <div>
+            {
+                winner != ""?(<h1>player {player==1? "🐵": "🦁"} win</h1>):null
+
+            }
+        </div>
+        <div className='baord'>
+            <div className='row'>
+                <div className='box' onClick={()=>{
+                    play(1)
+                }}>{board[1]}</div>
+                <div className='box'onClick={()=>{
+                    play(2)
+                }}>{board[2]}</div>
+                <div className='box' onClick={()=>{
+                    play(3)
+                }}>{board[3]}</div>
+            </div>
+            <div className='row'>
+                <div className='box' onClick={()=>{
+                    play(4)
+                }}>{board[4]}</div>
+                <div className='box' onClick={()=>{
+                    play(5)
+                }}>{board[5]}</div>
+                <div className='box' onClick={()=>{
+                    play(6)
+                }}>{board[6]}</div>
+            </div>
+            <div className='row'>
+                <div className='box' onClick={()=>{
+                    play(7)
+                }}>{board[7]}</div>
+                <div className='box' onClick={()=>{
+                    play(8)
+                }}>{board[8]}</div>
+                <div className='box' onClick={()=>{
+                    play(9)
+                }}>{board[9]}</div>
+            </div>
+      
+
+        </div>
+        <button className='btn' onClick={reset}>reset</button>      
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
+import Home from "./Home/Home";
 
 const root=ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-<h1>tic tac toe</h1>
+<Home/>
 )
